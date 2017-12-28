ee7a46bf6f6a47d0061e2a4e4db553c811dcfc1c
pc379 <pc379@cornell.edu>
2017-12-28 10:55:33 -0800

    fix: changed code to be more efficient

--- js-lessons/click.js
+++ js-lessons/click.js
@@ -1,4 +1,14 @@
 var button = document.querySelector("button");
+
+button.addEventListener("click", function(){
+	document.body.classList.toggle("purple");
+})
+
+
+
+
+/* Alternate Solution
+var button = document.querySelector("button");
 var body = document.querySelector("body");
 var flip = true;
 button.addEventListener("click", function(){
@@ -10,3 +20,4 @@ button.addEventListener("click", function(){
 	}
 	flip = !flip;
 });
+*/
